4f04bc2f2d79cec8feb0ef36508bb63c09a64bae
Kushagra Ambashta <2023ucp1850@mnit.ac.in>
2024-10-07 15:18:59 +0530

    Added cpp file.

---
+++ a.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define n 3
+
+using namespace std;
+
+int dotProduct(int vA[], int vB[])
+{
+
+    int product = 0;
+
+    // Loop for calculate dot product
+    for (int i = 0; i < n; i++)
+
+        product = product + vA[i] * vB[i];
+    return product;
+}
+
+
+int main()
+{
+
+    int vA[] = { 4, -2, 3 };
+    int vB[] = { 1, 3, 7 };
+
+    cout << "Dot product:";
+    cout << dotProduct(vA, vB) << endl;
+
+}
+
