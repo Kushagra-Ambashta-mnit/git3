256ae8162d5c4cea87b9475713cd5d917de7141b
Kushagra <2023ucp1850@mnit.ac.in>
2024-10-14 14:50:45 +0530

    updating code file in parallel

--- a.cpp
+++ a.cpp
@@ -22,7 +22,7 @@ int main()
     int vA[] = { 4, -2, 3 };
     int vB[] = { 1, 3, 7 };
 
-    cout << "Dot product:";
+    cout << "Dot product is actually equal to = ";
     cout << dotProduct(vA, vB) << endl;
 
 }
